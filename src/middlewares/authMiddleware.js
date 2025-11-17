cd02b8434914f67506c3f4ba62cac35c268c162b
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-17 17:15:17 +0530

    Added sigin with zod validations and added isauth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,49 @@
+import { StatusCodes } from "http-status-codes";
+import { customErrorResponse, internalErrorResponse } from "../utils/common/responseObject.js";
+import { JWT_SECRET } from "../config/serverConfig";
+import jwt from "jsonwebtoken.js";
+import userRepository from "../repositories/userRepository";
+
+export const isAunthenticated = async (req, res, next) => {
+    try {
+        const token = req.headers['x-access-token'];
+
+        if(!token) {
+            return res.status(StatusCodes.FORBIDDEN).json(
+                customErrorResponse({
+                    explanation: 'Invalid data sent from the client',
+                    message: 'No auth token provided'
+                })
+            );
+        }
+
+        const response = jwt.verify(token, JWT_SECRET);
+
+        if(!response) {
+            return res.status(StatusCodes.FORBIDDEN).json(
+                customErrorResponse({
+                    explanation: 'Invalid data sent from the client',
+                    message: 'Invalid auth token provided'
+                })
+            );
+        }
+
+        const user = await userRepository.getById(response.id);
+        req.user = user.id;
+        next();
+
+    } catch (error) {
+        console.log('Auth middleware error', error);
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(StatusCodes.FORBIDDEN).json(
+                customErrorResponse({
+                    explanation: 'Invalid data sent from the client',
+                    message: 'Invalid auth token provided'
+                })
+            );
+        }
+        return res
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json(internalErrorResponse(error));
+    }
+}
