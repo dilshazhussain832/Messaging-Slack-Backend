817a8be9c4915d46342f0c377b9a1d60a97d7adb
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-17 20:11:10 +0530

    Added workspace repo functions

---
+++ src/repositories/channelRepository.js
@@ -0,0 +1,10 @@
+import Channel from "../schema/channel.js";
+import crudRepository from "./crudRepository.js";
+
+const channelRepository = {
+    ...crudRepository(Channel),
+
+    
+};
+
+export default channelRepository;
