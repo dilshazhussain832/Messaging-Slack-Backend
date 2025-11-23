e27ecab9c317281da4c7acfb382f962f80f2d4df
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-23 20:43:35 +0530

    Added cors

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ import { PORT } from './config/serverConfig.js';
 import ChannelSocketHandlers from './controllers/channelSocketController.js';
 import MessageSocketHandlers from './controllers/messageSocketController.js';
 import apiRouter from './routes/apiRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
 const server = createServer(app);
 const io = new Server(server);
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
